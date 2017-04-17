d244069742ff9232a311f00815123491f69a2404
Brenna <bcmdarroch@gmail.com>
2017-04-17 15:23:58 -0700

    set up OAuth n OmniAuth, changed user model, wrote user create_from_github method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
+  validates :uid, presence: true, uniqueness: true
+  validates :provider, presence: true
   validates :username, uniqueness: true, presence: true
+
+  def self.create_from_github(auth_hash)
+    @user = User.create( uid: auth_hash['uid'],
+    provider: auth_hash['provider'],
+    username: auth_hash['info']['name'],
+    email: auth_hash['info']['email'] )
+  end
 end
