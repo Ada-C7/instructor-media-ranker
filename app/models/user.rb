1d301e92e5dac8c370a329d59cc50fe6e85651dd
Regan Huff <regan.huff@gmail.com>
2017-04-17 16:23:20 -0700

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,17 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   # validates :username, uniqueness: true, presence: true
-  # validates :uid, presence: true
-  # validates :provider, presence: true
-  # validates :email, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
+  validates :email, presence: true
 
   def self.create_from_github(auth_hash)
     user = User.new
     user.uid = auth_hash["uid"]
     user.provider = auth_hash["provider"]
-    user.username = auth_hash["info"]["name"] || "Stanley"
     user.email = auth_hash["info"]["email"]
+    user.save
+    user.username = auth_hash["info"]["name"] || "User#{user.id}"
     user.save ? user : nil
   end
 end
