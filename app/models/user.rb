ab4a53cfc0e7b65113870156a73978c838ab9cb1
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-04-17 15:09:49 -0700

    sessions controller create and class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.create_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.provider = auth_hash["provider"]
+    user.name = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+    user.save
+    return user
+  end
+
 end
