9c0815f6eaaa61c7c048f4069346f967fdce86a7
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-04-17 15:07:16 -0700

    migrated the db to add new info from github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
 
-  def create
+  def login
     auth_hash = request.env['omniauth.auth']
     # raise  # this raise will raise an error and let you see what is in the auth_hash ☺ Don’t want this all the time
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
 
-  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#login"
 
   # Build the category routes for albums, books and movies
   category_constraints = { category: /(albums)|(books)|(movies)/}
