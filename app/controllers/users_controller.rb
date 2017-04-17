ab5ecee9063600dba86eb8a05dcf4a08168ce1b6
Cara Comfort <cara.comfort10@gmail.com>
2017-04-17 16:11:48 -0700

    Add functionality to log in existing users and update log in button path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,20 +11,37 @@ class UsersController < ApplicationController
   def login
     auth_hash = request.env['omniauth.auth']
 
-    user = User.from_github(auth_hash)
+    user = User.find_by(
+      oauth_provider: params[:provider],
+      oauth_uid: auth_hash["uid"]
+    )
 
-    if user.save
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, new user #{user.username}"
-    else
-      flash[:failure] = "Could not log in"
-      user.errors.messages.each do |field, problem|
-        flash[:field] = problem.join(', ')
+    if user.nil?
+      # build a new user
+      user = User.from_github(auth_hash)
+
+      if user.save
+        session[:user_id] = user.id
+        flash[:status] = :success
+        flash[:result_text] = "Welcome, new user #{user.username}"
+        redirect_to root_path
+      else
+        flash.now[:status] = :failure
+        flash.now[:result_text] = "Could not log in"
+        flash.now[:messages] = user.errors.messages
+        # flash[:failure] = "Could not log in"
+        # user.errors.messages.each do |field, problem|
+        #   flash[:field] = problem.join(', ')
+        # end
+        # Go back to the login page? DOES THIS WORK?
+        redirect_to "/auth/github"
       end
+    else
+      # log in existing user
+      session[:user_id] = user.id
+      flash[:status] = :success
+      flash[:result_text] = "Welcome back, user #{user.username}"
+      redirect_to root_path
     end
-
-    redirect_to root_path
-
-
   end
 end
