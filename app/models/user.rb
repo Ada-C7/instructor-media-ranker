8252a822cfbcc84b866f4c4b33dc0333a9f76ac9
Ashtn <ashtonehorton@gmail.com>
2017-04-17 15:24:29 -0700

    added uid, provider, name and email fields to user schema, created model method to create new user from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.create_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash['uid']
+    user.provider = auth_hash['provider']
+    user.name = auth_hash['info']['name']
+    user.email = ['info']['email']
+
+    user.save ? user : nil
+  end
 end
