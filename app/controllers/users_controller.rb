378f863b7c7c78bf00eac82363612a89a2f429ac
Cara Comfort <cara.comfort10@gmail.com>
2017-04-17 15:40:21 -0700

    Add columns to users db, write class method in users model, and login method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     render_404 unless @user
   end
+
+  def login
+    auth_hash = request.env['omniauth.auth']
+
+    user = User.from_github(auth_hash)
+
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, new user #{user.username}"
+    else
+      flash[:failure] = "Could not log in"
+      user.errors.messages.each do |field, problem|
+        flash[:field] = problem.join(', ')
+      end
+    end
+
+    redirect_to root_path
+
+
+  end
 end
