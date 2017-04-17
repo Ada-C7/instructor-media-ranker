f5e7105f22071540eff21ffa16e5838dbd9313fe
Janice Lichtman <jalichtman@gmail.com>
2017-04-17 15:04:13 -0700

    added binding_of_caller gem AND route for /auth/:provider/callback

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,7 @@ Rails.application.routes.draw do
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote'
 
   resources :users, only: [:index, :show]
+
+  get "/auth/:provider/callback", to: "users#auth_callback"
+
 end
