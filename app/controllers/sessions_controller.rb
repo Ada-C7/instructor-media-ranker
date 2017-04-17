26628c1e85b4dee9861d325bfce06b8ce0bcabe5
Spatterjaaay <aja.pursova@gmail.com>
2017-04-17 15:42:42 -0700

    added logout method to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,10 @@ class SessionsController < ApplicationController
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
   end
 
   # def login
