5bc1f235d9ae8f6d26913fc07ef7b8e59fa7faa2
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-17 16:11:49 -0700

    added functionality to login as existing users and update login button path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,29 @@ class UsersController < ApplicationController
   def login
     auth_hash = request.env['omniauth.auth']
 
-    user = User.from_github(auth_hash)
-    if user.save
-      session[:user_id] = user.id
-      flash[:message] = "Successfully logged in as new user #{user.username}"
-    else
-      flash[:message] = "Could not log in"
-      user.errors.messages.each do |field, problem|
-        flash[:field] = problem.join(', ')
+    user = User.find_by(
+      oauth_provider: params[:provider],
+      oauth_uid: auth_hash["uid"])
+    if user.nil?
+      user = User.from_github(auth_hash)
+      if user.save
+        session[:user_id] = user.id
+        flash[:status] = :success
+        flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+        redirect_to root_path
+      else
+        flash.now[:status] = :failure
+        flash.now[:result_text] = "Could not log in"
+        flash.now[:messages] = user.errors.messages
+        redirect_to "/auth/github", status: :bad_request
+        #go back to log in page?
+        return
+        end
       end
+    else
+      session[:user_id] = user.id
+      flash[:status] = :success
+      flash[:result_text] = "Successfully logged in as returning #{user.username}"
+      redirect_to root_path
     end
-
-    redirect_to root_path
-  end
 end
