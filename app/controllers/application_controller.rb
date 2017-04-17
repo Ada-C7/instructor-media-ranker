f2e5cf05c47bbbae3d94b12f1cc9182f4cc8ccba
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-17 16:49:51 -0700

    wrote require_login method and a filter so that users can't see more than root page unless logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login, except: [:root, :login]
+
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
@@ -12,6 +14,15 @@ private
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_login
+    find_user
+    if @login_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = "You must be logged in to see that page"
+      redirect_to root_path
     end
   end
 end
