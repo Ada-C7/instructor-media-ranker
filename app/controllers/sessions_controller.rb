4f52eb6e1c6384355db3b874a9e12246d354e27d
Regan Huff <regan.huff@gmail.com>
2017-04-17 15:54:47 -0700

    Add code for Sessions create and User create_from_github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,22 @@
 class SessionsController < ApplicationController
 
-  def create 
+  def create
+    auth_hash = request.env['omniauth.auth']
+
+    user = User.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+
+    if user.nil?
+      user = User.create_from_github(auth_hash)
+
+      if user.nil?
+        flash[:error] = "Could not log in"
+        redirect_to root_path
+      end
+    end
+
+    session[:user_id] = user.id
+    flash[:success] = "Logged in successfully as #{user.username} with ID #{user.id}"
+    redirect_to root_path
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   # validates :uid, presence: true
   # validates :provider, presence: true
   # validates :email, presence: true
+
+  def self.create_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.provider = auth_hash["provider"]
+    user.username = auth_hash["info"]["name"] || "Stanley"
+    user.email = auth_hash["info"]["email"]
+    user.save ? user : nil
 end
