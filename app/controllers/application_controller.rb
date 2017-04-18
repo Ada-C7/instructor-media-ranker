e84938b94571f60405d8bb201790e6b0dfdbec3f
Spatterjaaay <aja.pursova@gmail.com>
2017-04-18 10:26:17 -0700

    created reaquire login action in application controller, put skip before action in works controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :require_login
   before_action :find_user
+
+  def require_login
+    # see if we have logged in user (probably using session)
+    if !session[:user_id]
+    # if not logged in:
+    # show message about not being logged in and to the root route
+      flash[:warning] = "You must be logged in to view thing page"
+      redirect_to root_path
+    end
+    # let it be if there's someone logged in
+  end
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
