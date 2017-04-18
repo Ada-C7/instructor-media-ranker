ac3e445aab144deaa9a459bc084f9817b7e68dd8
Ashtn <ashtonehorton@gmail.com>
2017-04-18 10:27:53 -0700

    eod monday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  before_action :require_login
+
 
   def render_404
     # DPR: supposedly this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
   end
+
+
 
 private
   def find_user
