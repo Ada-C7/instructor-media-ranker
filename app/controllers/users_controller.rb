d40cd18888a5f84c7e98565533a926e44548556c
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-18 10:39:48 -0700

    added column to works table with user_id, and relationships in models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login, except: [:login]
+  # before_action :require_login, except: [:login]
 
   def index
     @users = User.all
---
+++ db/migrate/20170418170105_add_user_id_column_to_works.rb
@@ -0,0 +1,5 @@
+class AddUserIdColumnToWorks < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :works, :user
+  end
+end
