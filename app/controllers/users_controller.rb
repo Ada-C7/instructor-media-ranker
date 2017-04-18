7daeee866b59efbe4695e003b12c9f68c10e1a9e
EricaJCase <ericajcase@gmail.com>
2017-04-18 10:04:27 -0700

    added user-work relationship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,40 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     render_404 unless @user
   end
+
+  def login
+    auth_hash = request.env['omniauth.auth']
+
+    # Attempt to find these credentials in our DB
+    user = User.find_by(oauth_provider: params[:provider],
+    oauth_uid: auth_hash["uid"])
+
+    if user.nil?
+      # Don't know this user - build a new one
+      user = User.from_github(auth_hash)
+      if user.save
+        session[:user_id] = user.id
+        flash[:result_text] = "Successfully logged in as new user #{user.username}"
+      else
+        flash[:result_text] = "Could not log in"
+        user.errors.messages.each do |field, problem|
+          flash[:field] = problem.join(', ')
+        end
+      end
+
+    else
+      # Welcome back!
+      session[:user_id] = user.id
+      flash[:result_text] = "Welcome back, #{user.username}"
+    end
+
+    redirect_to root_path
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out"
+    redirect_to root_path
+  end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,6 @@
 class WorksController < ApplicationController
+  before_action :require_login, except: [:root]
+
   # We should always be able to tell what category
   # of work we're dealing with
   before_action :category_from_url, only: [:index, :new, :create]
