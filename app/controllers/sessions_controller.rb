20efcb43158d45ed12d9243903d9b4c1732e2b47
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-04-30 12:18:58 -0700

    Added require_login before_action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def login_form
   end
 
@@ -20,29 +22,6 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     session[:user_name] = user.name
   end
-
-  # def login
-  #   username = params[:username]
-  #   if username and user = User.find_by(username: username)
-  #     session[:user_id] = user.id
-  #     flash[:status] = :success
-  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-  #   else
-  #     user = User.new(username: username)
-  #     if user.save
-  #       session[:user_id] = user.id
-  #       flash[:status] = :success
-  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-  #     else
-  #       flash.now[:status] = :failure
-  #       flash.now[:result_text] = "Could not log in"
-  #       flash.now[:messages] = user.errors.messages
-  #       render "login_form", status: :bad_request
-  #       return
-  #     end
-  #   end
-  #   redirect_to root_path
-  # end
 
   def logout
     session[:user_id] = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
 
 
   def self.create_from_github(auth_hash)
-  user = User.new
-  user.uid = auth_hash["uid"]
-  user.provider = auth_hash["provider"]
-  user.name = auth_hash["info"]["name"]
-  user.email = auth_hash["info"]["email"]
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.provider = auth_hash["provider"]
+    user.name = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
 
-  user.save ? user : nil
+    user.save ? user : nil
   end
 end
