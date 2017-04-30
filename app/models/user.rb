705381cb3a74e0576b162770ea0dfc8c8120de79
Kayla <kubicke.kayla@gmail.com>
2017-04-30 16:28:02 -0700

    Cleaning up files.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
+  has_many :works
+
   validates :username, uniqueness: true, presence: true
 
   def self.from_github(auth_hash)
