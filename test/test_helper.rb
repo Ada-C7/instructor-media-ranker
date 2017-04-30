a367aec881fe3f33aefce72df57615dbdb68ea2c
Hyunji Kim <ricecakemonster@gmail.com>
2017-04-30 14:47:23 -0700

    submitting homework

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,4 +23,24 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+  def setup
+  #Make OmniAuth accept mocked data in tests
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user) #producing mock data for the test
+    return {
+      provider:user.oauth_provider,
+      uid: user.oauth_uid,
+      info: {
+        email: user.email,
+        nickname: user.username
+      }
+    }
+  end
+
+  def login(user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get login_path(:github)
+  end
 end
