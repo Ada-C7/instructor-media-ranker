4f916765ab895997eeca74da188755977535dbc3
Bo Trethewey <botrethewey@gmail.com>
2017-04-30 22:24:20 -0700

    clean up styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def require_login
     if !session[:user_id]
       flash[:status] = :failure
-      flash[:result_text] = "You must be logged in to view this page"
+      flash[:result_text] = "You must be logged in to use features"
       redirect_to root_path
     end
   end
