000f32c02c29349ea006d9c281fe43dcc5795f71
Haby Randall <sow.haby@gmail.com>
2017-05-01 07:39:52 -0700

    Added create user id session to works controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,8 @@ class ApplicationController < ActionController::Base
 
 private
   def find_user
-    if session[:user_id]
-      @login_user = User.find_by(id: session[:user_id])
+    unless session[:user_id].nil?
+      @logged_in_user = User.find_by(id: session[:user_id])
     end
   end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,7 @@ class WorksController < ApplicationController
   # of work we're dealing with
   before_action :category_from_url, only: [:index, :new, :create]
   before_action :category_from_work, except: [:root, :index, :new, :create]
+  before_action :require_ownership, only: [:edit, :update, :destroy]
   before_action :require_login, except: [:root]
 
   def root
@@ -23,6 +24,7 @@ class WorksController < ApplicationController
 
   def create
     @work = Work.new(media_params)
+    @work.user_id = session[:user_id]
     if @work.save
       flash[:status] = :success
       flash[:result_text] = "Successfully created #{@media_category.singularize} #{@work.id}"
